630d09c29d21de45a395f53810ad4b0b6db176bb
Benjamin Scarbrough <Scarbrough.ben@gmail.com>
2021-09-21 16:27:32 -0700

    made skills in the about section easier to add

--- src/app/parts/about/about.component.ts
+++ src/app/parts/about/about.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class AboutComponent implements OnInit {
 
+  technologies = ["Java", "Python", "JavaScript", "Angular", "SQL", "HTML", "CSS", "Object Oriented Programming", "Node.js",
+    "Express.js", "AWS", "Full Stack Development", "Driven", "Innovative", "Strategic", "Curious", "Proactive", "Analytical",
+    "Interpersonal Skills", "Collaborative Team Player", "Problem Solving", "Critical Thinking", "Creative Thinking", "Multitasking",
+    "Priority Management", "Spanish Fluency", "Microsoft Office"]
+
   my_image = "../../../assets/Ben_Mount Taranaki.png";
   ups_link = "https://www.pugetsound.edu";
   covid_link = "https://www.cdc.gov/coronavirus/2019-nCoV/index.html";
